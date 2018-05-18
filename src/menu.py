b682a7e102044d8d79e95cfc8c435be5a1316669
Bartłomiej Klocek <barthap10@gmail.com>
2018-05-18 17:08:35 +0200

    Convert spaces to tabs
    Small cleanup in game code

--- src/menu.py
+++ src/menu.py
@@ -1,5 +1,5 @@
 import pygame
-import pygameMenu                # This imports classes and other things
+import pygameMenu  # This imports classes and other things
 from pygameMenu.locals import *  # Import constants (like actions)
 
 from constants import *
@@ -7,65 +7,67 @@ from scene import BaseScene
 from game import GameScene
 import pygameMenu.config_textmenu as _cfg
 
+
 # Main Menu scene
 class supermenu(pygameMenu.TextMenu):
 	def __init__(self,
-                 surface,
-                 window_width,
-                 window_height,
-                 font,
-                 title,
-                 draw_text_region_x=_cfg.TEXT_DRAW_X,
-                 text_centered=_cfg.TEXT_CENTERED,
-                 text_color=_cfg.TEXT_FONT_COLOR,
-                 text_fontsize=_cfg.MENU_FONT_TEXT_SIZE,
-                 text_margin=_cfg.TEXT_MARGIN,
-                 **kwargs):
-		super(supermenu,self).__init__(
-                surface,
-                window_width,
-                window_height,
-                font,
-                title,
-                draw_text_region_x,
-                text_centered,
-                text_color,
-                text_fontsize,
-                text_margin,
-				** kwargs)
+	             surface,
+	             window_width,
+	             window_height,
+	             font,
+	             title,
+	             draw_text_region_x=_cfg.TEXT_DRAW_X,
+	             text_centered=_cfg.TEXT_CENTERED,
+	             text_color=_cfg.TEXT_FONT_COLOR,
+	             text_fontsize=_cfg.MENU_FONT_TEXT_SIZE,
+	             text_margin=_cfg.TEXT_MARGIN,
+	             **kwargs):
+		super(supermenu, self).__init__(
+			surface,
+			window_width,
+			window_height,
+			font,
+			title,
+			draw_text_region_x,
+			text_centered,
+			text_color,
+			text_fontsize,
+			text_margin,
+			**kwargs)
+
 
 class MenuScene(BaseScene):
-    def __init__(self, app):
-        super().__init__(app)
+	def __init__(self, app):
+		super().__init__(app)
 
-        # Main Menu
-        self.menu = pygameMenu.Menu(app.graphics.screen, window_width=SCREEN_WIDTH, window_height=SCREEN_HEIGHT,
-                               font=pygameMenu.fonts.FONT_NEVIS,
-                               title='Menu Gry', bgfun=None, dopause=False)
+		# Main Menu
+		self.menu = pygameMenu.Menu(app.graphics.screen, window_width=SCREEN_WIDTH, window_height=SCREEN_HEIGHT,
+		                            font=pygameMenu.fonts.FONT_NEVIS,
+		                            title='Menu Gry', bgfun=None, dopause=False)
 
-        # Show the rules
-        self.help_menu = supermenu(app.graphics.screen, window_width=SCREEN_WIDTH, window_height=SCREEN_HEIGHT,
-                                        font=pygameMenu.fonts.FONT_FRANCHISE,
-                                        onclose=PYGAME_MENU_DISABLE_CLOSE,
-                                        title='Help', dopause=False,
-                                        menu_color_title=(120, 45, 30),
-                                        menu_color=(30, 50, 107))
+		# Show the rules
+		self.help_menu = supermenu(app.graphics.screen, window_width=SCREEN_WIDTH, window_height=SCREEN_HEIGHT,
+		                           font=pygameMenu.fonts.FONT_FRANCHISE,
+		                           onclose=PYGAME_MENU_DISABLE_CLOSE,
+		                           title='Help', dopause=False,
+		                           menu_color_title=(120, 45, 30),
+		                           menu_color=(30, 50, 107))
 
-    def setup(self):
-        self.app.graphics.clear_screen()
+	def setup(self):
+		self.app.graphics.clear_screen()
 
-        self.menu.add_option('Play', self.__go_play)  #TODO: Add timer submenu
-        self.menu.add_option('Rules', self.help_menu)
-        self.menu.add_option('Exit', self.app.exit)
+		self.menu.add_option('Play', self.__go_play)  # TODO: Add timer submenu
+		self.menu.add_option('Rules', self.help_menu)
+		self.menu.add_option('Exit', self.app.exit)
 
-        HELP = open("resources/rules.txt","r")
+		HELP = open("resources/rules.txt", "r")
 
-        for line in HELP:
-            self.help_menu.add_line(line)  # Add line
-        self.help_menu.add_option('Return to Menu', PYGAME_MENU_BACK)  # Add option
+		for line in HELP:
+			self.help_menu.add_line(line)  # Add line
+		self.help_menu.add_option('Return to Menu', PYGAME_MENU_BACK)  # Add option
 
-    def update(self, events):
-        self.menu.mainloop(events)
+	def update(self, events):
+		self.menu.mainloop(events)
 
-    def __go_play(self):
-        self.app.switch_scene(GAME)
+	def __go_play(self):
+		self.app.switch_scene(GAME)
--- src/scene.py
+++ src/scene.py
@@ -7,45 +7,45 @@ from constants import *
 # Base class for game scene (Intro, Menu, Game etc.). All states should derive from it
 # TODO: Make it abstract
 class BaseScene:
-    def __init__(self, app: 'App'):
-        self.app = app
+	def __init__(self, app: 'App'):
+		self.app = app
 
-    def setup(self):
-        pass
+	def setup(self):
+		pass
 
-    def update(self, events):
-        pass
+	def update(self, events):
+		pass
 
-    # something like destructor, called when state is changing or app is quitting
-    def destroy(self):
-        pass
+	# something like destructor, called when state is changing or app is quitting
+	def destroy(self):
+		pass
 
 
 class SceneManager:
-    def __init__(self, app: 'App'):
-        self._scenes: Dict[str, BaseScene] = {}
-        self.current: BaseScene = None
-        self.app = app
+	def __init__(self, app: 'App'):
+		self._scenes: Dict[str, BaseScene] = {}
+		self.current: BaseScene = None
+		self.app = app
 
-    # Call this after pygame.init()
-    def setup(self, initial: str):
-        self.go(initial)
+	# Call this after pygame.init()
+	def setup(self, initial: str):
+		self.go(initial)
 
-    def get_scene_object(self, scene_name: str) -> BaseScene:
-        if scene_name in self._scenes.keys():
-            scene = self._scenes[scene_name]
+	def get_scene_object(self, scene_name: str) -> BaseScene:
+		if scene_name in self._scenes.keys():
+			scene = self._scenes[scene_name]
 
-        else:
-            scene_class = pydoc.locate(scene_name)
-            if scene_class is None:
-                raise RuntimeError("Couldn't load scene " + scene_name)
-            scene = scene_class(self.app)
-            print("Loaded", scene_class.__name__)
+		else:
+			scene_class = pydoc.locate(scene_name)
+			if scene_class is None:
+				raise RuntimeError("Couldn't load scene " + scene_name)
+			scene = scene_class(self.app)
+			print("Loaded", scene_class.__name__)
 
-        return scene
+		return scene
 
-    def go(self, new_scene: str):
-        if self.current is not None:
-            self.current.destroy()
-        self.current = self.get_scene_object(new_scene)
-        self.current.setup()
+	def go(self, new_scene: str):
+		if self.current is not None:
+			self.current.destroy()
+		self.current = self.get_scene_object(new_scene)
+		self.current.setup()
